655fbf96f527fdf84941a42f30e6cd62085bcc09
Sravan Ravi <sravan953@gmail.com>
2015-05-24 23:05:37 +0530

    Added ShareActionProvider

--- app/src/main/java/com/biryanistudio/spotifystreamer/Activity/ArtistsActivity.java
+++ app/src/main/java/com/biryanistudio/spotifystreamer/Activity/ArtistsActivity.java
@@ -6,8 +6,12 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -110,5 +114,22 @@ public class ArtistsActivity extends ActionBarActivity implements AdapterView.On
             else
                 Toast.makeText(context, "No artists found!", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        String shareText = "Check out Spotify Streamer on https://github.com/sravan953/Spotify-Streamer";
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        mShareActionProvider.setShareIntent(shareIntent);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
